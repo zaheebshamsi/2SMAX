18844c5081f73d931cdde2b1ba8a1007e2549530
ZShamsi <zaheeb.shamsi@microfocus.com>
2021-06-14 21:27:14 +0530

    whole new file with different attire. -2 added scrollbar with canvas and dropdown and labels

--- 2smaxv1.py
+++ 2smaxv1.py
@@ -1,14 +1,12 @@
-try:
-    import tkinter as tk  # python 3
-    from tkinter import font as tkfont  # python 3
-    from PIL.ImageTk import PhotoImage
-    import tkinter.messagebox
-    import os
-except ImportError:
-    import Tkinter as tk  # python 2
-    import tkFont as tkfont  # python 2
+from tkinter.ttk import Combobox
 
-    os.system('pip install PIL')
+import requests
+from requests import get, post
+import sys
+import tkinter as tk
+from tkinter import font as tkfont
+import tkinter.messagebox
+from json import dumps, loads, load, dump
 
 
 class ToSMAXApp(tk.Tk):
@@ -16,7 +14,7 @@ class ToSMAXApp(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+        self.title_font = tkfont.Font(family='Helvetica', size=12, weight="bold", slant="italic")
 
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
@@ -40,25 +38,34 @@ class ToSMAXApp(tk.Tk):
             # the one on the top of the stacking order
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
-        print(self.frames)
 
         self.show_frame("ApiPage")
 
     def show_frame(self, page_name):
         """Show a frame for the given page name"""
-        print(page_name)
         frame = self.frames[page_name]
         frame.tkraise()
 
 
 class ApiPage(tk.Frame):
+    def fetch_source_data_button_func2(self,
+                                       server_name_ip_textbox_source,
+                                       username_textbox_source,
+                                       tenant_id_textbox_source,
+                                       password_textbox_source,
+                                       module_textbox_source,
+                                       layout_textbox_source):
+        print(server_name_ip_textbox_source.get("1.0", "end-1c"))
+        return server_name_ip_textbox_source.get("1.0", "end-1c")
+
     def __init__(self, parent, controller):
-        def fetch_source_data_button_func(server_name_ip_textbox_source,
-                                          username_textbox_source,
-                                          tenant_id_textbox_source,
-                                          password_textbox_source,
-                                          module_textbox_source,
-                                          layout_textbox_source):
+
+        def fetch_source_data_button_func1(server_name_ip_textbox_source,
+                                           username_textbox_source,
+                                           tenant_id_textbox_source,
+                                           password_textbox_source,
+                                           module_textbox_source,
+                                           layout_textbox_source):
             try:
                 server_name_ip = server_name_ip_textbox_source.get("1.0", "end-1c")
                 username = username_textbox_source.get("1.0", "end-1c")
@@ -69,17 +76,69 @@ class ApiPage(tk.Frame):
 
                 print(server_name_ip, username, password, tenant_id, module, layout)
                 print("This is inside seq")
+
+                if server_name_ip == "Enter the FQDN/IP of Server":
+                    server_name_ip = None
+                if username == "Enter the username":
+                    username = None
+                if password == "Enter the Password":
+                    password = None
+                if tenant_id == "Enter the Tenant ID":
+                    tenant_id = None
+                if module == "Module- Eg: Incident,Change":
+                    module = None
+                if layout == "":
+                    pass
+
+                if server_name_ip is None or username is None or password is None or \
+                        tenant_id is None or module is None or layout is None:
+                    tkinter.messagebox.showinfo("Source REST Call", "Entries can not be default text, "
+                                                                    "Please remove default text from the entry box")
+                    return False
+
+                url_token = "https://" + str(
+                    server_name_ip) + "/auth/authentication-endpoint/authenticate/login?TENANTID=" + \
+                            str(tenant_id)
+                payload = {"Login": username, "Password": password}
+                headers = {
+                    "Content-Type": "application/json",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Accept": "*/*",
+                    "Connection": "keep-alive"
+                }
+                response_token = post(url_token, data=dumps(payload), headers=headers, verify=False)
+                token = response_token.text
+
+                # ---------------Get the details after receiving the TOKEN---------------#
+
+                url_get = "https://" + str(server_name_ip) + "/rest/" + str(tenant_id) + "/ems/" + module + \
+                          "?layout=" + layout
+                headers = {
+                    "Content-Type": "application/json",
+                    "Cookie": "LWSSO_COOKIE_KEY={}".format(token)
+                }
+                response_data = get(url_get, headers=headers, verify=False)
+                print(response_data.status_code)
+                source_data_get = response_data.content.decode('ascii')
+                source_data_get_json = loads(source_data_get)
+
+                json_file = dumps(source_data_get_json, indent=4)
+                print(json_file)
+                print(type(json_file))
+                print(type(source_data_get_json))
+                return json_file
+
             except Exception as ssl:
                 print(ssl)
                 print(ssl.__class__)
-                tkinter.messagebox.showinfo("FATAL!!", ssl)
+                tkinter.messagebox.showwarning("FATAL!!", ssl)
 
         tk.Frame.__init__(self, parent, background='#1a2933')
         self.controller = controller
         # label = tk.Label(self, text="This is the start page", font=controller.title_font)
         # label.pack(side="top", fill="x", pady=10)
 
-        img = PhotoImage(file="C:/Users/ZShamsi/Downloads/zaheebimg.png")  # make sure to add "/" not "\"
+        # img = PhotoImage(file="C:/Users/ZShamsi/Downloads/zaheebimg.png")  # make sure to add "/" not "\"
         button1 = tk.Button(self, text="Go to Results Page", bg='powder blue', fg='black', relief='raised',
                             command=lambda: controller.show_frame("ResultsPage"))
         # button1.config(image=img)
@@ -172,12 +231,15 @@ class ApiPage(tk.Frame):
         # --------------------Button for API URL-------------------#
         fetch_source_data_button = tk.Button(self, relief='raised', text="↑ Fetch Source Data", height=2, width=30,
                                              command=lambda: [controller.show_frame("ResultsPage"),
-                                                              fetch_source_data_button_func(server_name_ip_textbox_source,
-                                                                                            username_textbox_source,
-                                                                                            tenant_id_textbox_source,
-                                                                                            password_textbox_source,
-                                                                                            module_textbox_source,
-                                                                                            layout_textbox_source)])
+                                                              ResultsPage.fetch_source_data_button_func(
+                                                                  self,
+                                                                  server_name_ip_textbox_source,
+                                                                  username_textbox_source,
+                                                                  tenant_id_textbox_source,
+                                                                  password_textbox_source,
+                                                                  module_textbox_source,
+                                                                  layout_textbox_source)])
+        fetch_source_data_button.place(relx=0.5, rely=0.40, anchor="center")
         # command=self.sequence(controller.show_frame("ResultsPage")))
 
         # fetch_source_data_button = tk.Button(self, relief='raised', text="↑ Fetch Source Data", height=2,
@@ -188,7 +250,6 @@ class ApiPage(tk.Frame):
         #                                                                                 password_textbox_source,
         #                                                                                 module_textbox_source,
         #                                                                                 layout_textbox_source)
-        fetch_source_data_button.place(relx=0.5, rely=0.40, anchor="center")
 
         fetch_destination_data_button = tk.Button(self, relief='raised', text="↑ Fetch Destination Data", height=2,
                                                   width=30)
@@ -206,15 +267,201 @@ class ApiPage(tk.Frame):
 
 
 class ResultsPage(tk.Frame):
+    @staticmethod
+    def fetch_source_data_button_func(server_name_ip_textbox_source,
+                                      username_textbox_source,
+                                      tenant_id_textbox_source,
+                                      password_textbox_source,
+                                      module_textbox_source,
+                                      layout_textbox_source):
+        try:
+            max_tries = 5
+            attempt = 0
+
+            server_name_ip = server_name_ip_textbox_source.get("1.0", "end-1c")
+            username = username_textbox_source.get("1.0", "end-1c")
+            password = password_textbox_source.get("1.0", "end-1c")
+            tenant_id = tenant_id_textbox_source.get("1.0", "end-1c")
+            module = module_textbox_source.get("1.0", "end-1c")
+            layout = layout_textbox_source.get("1.0", "end-1c")
+
+            print(server_name_ip, username, password, tenant_id, module, layout)
+            print("This is inside seq")
+
+            if server_name_ip == "Enter the FQDN/IP of Server":
+                server_name_ip = None
+            if username == "Enter the username":
+                username = None
+            if password == "Enter the Password":
+                password = None
+            if tenant_id == "Enter the Tenant ID":
+                tenant_id = None
+            if module == "Module- Eg: Incident,Change":
+                module = None
+            if layout == "":
+                pass
+
+            if server_name_ip is None or username is None or password is None or \
+                    tenant_id is None or module is None or layout is None:
+                tkinter.messagebox.showinfo("Source REST Call", "Entries can not be default text, "
+                                                                "Please remove default text from the entry box")
+                return False
+
+            url_token = "https://" + str(
+                server_name_ip) + "/auth/authentication-endpoint/authenticate/login?TENANTID=" + \
+                        str(tenant_id)
+            payload = {"Login": username, "Password": password}
+            headers = {
+                "Content-Type": "application/json",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept": "*/*",
+                "Connection": "keep-alive"
+            }
+            while attempt <= max_tries:
+                response_token = post(url_token, data=dumps(payload), headers=headers, verify=False)
+                token = response_token.text
+
+                # ---------------Get the details after receiving the TOKEN---------------#
+
+                url_get = "https://" + str(server_name_ip) + "/rest/" + str(tenant_id) + "/ems/" + module + \
+                          "?layout=" + layout
+                headers = {
+                    "Content-Type": "application/json",
+                    "Cookie": "LWSSO_COOKIE_KEY={}".format(token)
+                }
+                response_data = get(url_get, headers=headers, verify=False)
+                source_data_get = response_data.content.decode('utf-8')
+                source_data_get_json = loads(source_data_get)
+                json_file = dumps(source_data_get_json, indent=4)
+                with open("snow.json", "w") as outfile:
+                    outfile.write(json_file)
+
+                # ---------------Display JSON keys as labels---------------#
+                with open('snow.json') as env:
+                    data = load(env)
+                res = list()
+                for json_data_get_response in data['entities']:
+                    # print(json_data_get_response)
+                    for json_data_key in json_data_get_response['properties'].keys():
+                        res.append(json_data_key)
+                remove_duplicates_from_json = []
+                [remove_duplicates_from_json.append(x) for x in res if x not in remove_duplicates_from_json]
+                print(remove_duplicates_from_json)
+                # dumps(source_data_get_json, indent=4)
+
+        except Exception as ssl:
+            print(ssl)
+            print(ssl.__class__)
+            tkinter.messagebox.showwarning("FATAL!!", ssl)
+            exception_type, exception_object, exception_traceback = sys.exc_info()
+            filename = exception_traceback.tb_frame.f_code.co_filename
+            line_number = exception_traceback.tb_lineno
+            print("Exception type: ", exception_type)
+            print("File name: ", filename)
+            print("Line number: ", line_number)
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, background='#1a2933')
         self.controller = controller
-        label = tk.Label(self, text="This is page 1", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
+        self.task(controller)
+
+    def task(self, controller):
+        def myfunction(event):
+            canvas.configure(scrollregion=canvas.bbox("all"), width=517, height=570)  # 1179 full screen
+            print('in mf')
+
+        self.controller = controller
+        canvas = tk.Canvas(self, bg='powder blue')
+        frame_canvas = tk.Frame(canvas, bg='cyan')
+        myscrollbar = tk.Scrollbar(self, orient="vertical", command=canvas.yview)
+        canvas.configure(yscrollcommand=myscrollbar.set)
+
+        myscrollbar.pack(side="right", fill="y")
+        canvas.pack()
+        canvas.create_window((0, 0), window=frame_canvas, anchor='nw')
+        print('before mf')
+        frame_canvas.bind_all("<Configure>", myfunction)
+        print('after mf')
+
+        # label = tk.Labelframe_canvas(frame, text="This is Results Page", font=controller.title_font)
+        # label.pack(side="top", pady=10)
+        #
+        var = tk.StringVar()
+        label = tk.Label(frame_canvas, textvariable=var, font=controller.title_font)
+        var.set("Source API Variables")
+        label.grid(row=0, column=0)
+        # label.place(relx=0.2, rely=0.1, anchor="center")
+
+        var = tk.StringVar()
+        label = tk.Label(frame_canvas, textvariable=var, font=controller.title_font)
+        var.set("SMAX API Variables")
+        label.grid(row=0, column=3)
+        # label.place(relx=0.51, rely=0.1, anchor="center")
+
+        button = tk.Button(self, text="Go to the API page", relief='raised', height=2, width=30,
                            command=lambda: controller.show_frame("ApiPage"))
-        button.pack()
+        button.place(relx=0.87, rely=0.5, anchor="center")  # relx=0.8, rely=0.1
+
+        button_push = tk.Button(self, text="Push", relief='raised', height=2, width=30,
+                                command=lambda: controller.show_frame("ApiPage"))
+        button_push.place(relx=0.87, rely=0.4, anchor="center")  # relx=0.8, rely=0.1
+        # ResultsPage.loads_json()
+
+        # scrollbar_source = tk.Scrollbar(self, orient="vertical")
+        # scrollbar_source.pack(side='right', fill='y')
+
+        zzz = ['Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name',
+               'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id',
+               'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email',
+               'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name',
+               'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id', 'Email', 'Name', 'Id',
+               'Email']
+        x = 1
+        y = 0
+        for xxx in zzz:
+            textbox_source_variables = tk.Text(frame_canvas, height=2, width=25, fg='black', font=controller.title_font,
+                                               bg='powder blue')
+            textbox_source_variables.insert(tk.END, xxx)
+            # textbox_source_variables.place(relx=x, rely=y, anchor="center")
+            textbox_source_variables.grid(row=x, column=y, padx=10, pady=10)
+            textbox_source_variables.bindtags((textbox_source_variables, self, "all"))
+            x = x + 1
+
+        options = ["Ticket Number", "Ticket Age", "Email", "First Name", "Middle Name", "Last Name", "Emp ID",
+                   "Problem Description", "Technician Name", "Solution Description", "Resolution Time (min)"]
+
+        for i in range(len(zzz)):
+            drop = Combobox(frame_canvas, state='readonly', values=options, font="20")
+            drop.grid(row=i + 1, column=3, sticky="nsew", padx=10, pady=15)
+        # scrollbar_source.config(command=textbox_source_variables.yview)
+        # listbox = tk.Listbox(self, yscrollcommand=scrollbar_source.set, bg='#1a2933', fg='black', height=25,
+        #                      font=controller.title_font, relief='ridge', borderwidth=0, highlightthickness=0,
+        #                      background=self.cget("background"))
+        # for label_json_key in zzz:
+        #     i += 1
+        #     listbox.insert(tk.END, str(label_json_key))
+        # # listbox.pack(side=tk.LEFT, fill=tk.BOTH)
+        # listbox.place(relx=0.2, rely=0.58, anchor="center")
+        # listbox.bindtags((listbox, self, "all"))
+        # var = tk.StringVar()
+
+        # label_show_json_source_variable = tk.Label(self, textvariable=var, bd=5, relief='ridge',
+        #                                            # padx=5, pady=5,
+        #                                            width=20, bg='black', fg='white', font=controller.title_font)
+        # var.set(label_json_key)
+        # label_show_json_source_variable.place(relx=x, rely=y, anchor="center")
+        # y = y + 0.1
+        # # x = x + 0.05
+        # print(x, y)
+
+        # scrollbar_source.config(command=label_show_json_source_variable.yview)
+        # label.grid(row=i, column=1, sticky="nsew", padx=12, pady=12)
+        # label.bind("<Key>", update_size)
+
+    @staticmethod
+    def loads_json():
+        data = ResultsPage.fetch_source_data_button_func
+        print("This is data", data)
 
 
 class PageTwo(tk.Frame):
